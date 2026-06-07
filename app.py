2688c2795c929e081b091a1fec029300204a89cc
Sanskriti Gupta <guptasanskriti021@gmail.com>
2026-06-07 22:10:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
 
+st.set_page_config(page_title="Customer Churn Prediction")
+
+st.title("Customer Churn Prediction")
+
+st.write("Welcome to Customer Churn Prediction App")
+
+st.success("App deployed successfully!")
